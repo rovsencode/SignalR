78ab4cc1fa8ac7ddf4c975cc6dc7fbce88225105
rovsencode <rovshanakh@code.edu.az>
2023-03-17 15:16:01 +0400

    add chat

---
+++ FirelloProject/Controllers/ChatController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirelloProject.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
--- FirelloProject/Hubs/ChatHub.cs
+++ FirelloProject/Hubs/ChatHub.cs
@@ -4,6 +4,9 @@ namespace FirelloProject.Hubs
 {
     public class ChatHub:Hub
     {
-
+      public async  Task SendMessage(string user,string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",user, message);
+        }
     }
 }
