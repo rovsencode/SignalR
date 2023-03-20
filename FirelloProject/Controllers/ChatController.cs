704af8a24537f02574e02877df47b09eb8e6eb1e
rovsencode <rovshanakh@code.edu.az>
2023-03-20 23:56:33 +0400

    add IHubContext

--- FirelloProject/Controllers/ChatController.cs
+++ FirelloProject/Controllers/ChatController.cs
@@ -1,21 +1,33 @@
-﻿using FirelloProject.Models;
+﻿using FirelloProject.Hubs;
+using FirelloProject.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace FirelloProject.Controllers
 {
     public class ChatController : Controller
     {
         private readonly UserManager<AppUser> _userManager;
+        private readonly IHubContext<ChatHub> _hubContext;
 
-        public ChatController(UserManager<AppUser> userManager)
+        public ChatController(UserManager<AppUser> userManager, IHubContext<ChatHub> hubContext)
         {
             _userManager = userManager;
+            _hubContext = hubContext;
         }
         public IActionResult Chat()
         {
             ViewBag.Users = _userManager.Users.ToList();
             return View();
+        }
+
+        public async Task<IActionResult> ShowAlert(string userId)
+        {
+            AppUser user = await _userManager.FindByIdAsync(userId);
+            _hubContext.Clients.Client(user.ConnectionId).SendAsync("ShowAlert", "salam");
+
+            return RedirectToAction("chat");
         }
     }
 }
